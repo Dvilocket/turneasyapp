51b22b5ed2a86b6d8335b333eb44161d6fffbb4e
miguel <miguel.ramirez2@utp.edu.co>
2025-04-10 15:53:17 -0500

    cambios nuevos

--- src/appointment/decorator/IsTimeGreaterOrEqualCurrentConstraint.ts
+++ src/appointment/decorator/IsTimeGreaterOrEqualCurrentConstraint.ts
@@ -5,20 +5,23 @@ export class IsTimeGreaterOrEqualCurrentConstraint implements ValidatorConstrain
     validate(serviceTime: string, args: ValidationArguments) {
         if (!serviceTime) return false;
 
-        const now = new Date();
-        const currentHour = now.getHours();
-        const currentMinute = now.getMinutes();
-
         const [hour, minute] = serviceTime.split(':').map(Number);
 
-
-        if (hour > currentHour) {
-            return true;
-        } else if (hour === currentHour && minute >= currentMinute) {
-            return true;
+        if (hour < 6 || hour > 23) {
+            return false;
         }
 
-        return false; 
+        const object: any = args.object;
+        const serviceDate: string = object.fecha_servicio;
+
+        if (!serviceDate) return false;
+
+        const [year, month, day] = serviceDate.split('-').map(Number);
+        const serviceDateTime = new Date(year, month - 1, day, hour, minute, 0);
+
+        const now = new Date();
+
+        return serviceDateTime >= now;
     }
 
     defaultMessage(args: ValidationArguments) {
